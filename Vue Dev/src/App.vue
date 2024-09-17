da83b22244f8b8f302735dd162517d189e7a3733
r.gevich <r.gevich@labirint-t.ru>
2024-09-17 10:34:56 +0300

    Vue Start

---
+++ Vue Dev/src/App.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+data() {
+  return {
+    info: 'Title',
+    someInfo: 'Anons of message'
+  }
+}
+}
+</script>
+
+<template>
+<h3>{{info}}</h3>
+  <p>{{someInfo}}</p>
+
+  <button type="button" @click="this.info = 'Some New'" class="btn">Отправить</button>
+</template>
+
+
+<style scoped>
+
+h3 {
+  display:flex;
+  text-align: center;
+  color: green;
+  justify-content: center;
+  font-weight: lighter;
+}
+
+p {
+  color: rgb(203, 31, 31);
+  display:flex;
+  text-align: center;
+  justify-content: center;
+}
+
+.btn {
+  display:flex;
+  text-align: center;
+  justify-content: center;
+  width: 100%;
+}
+
+</style>
